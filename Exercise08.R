8a19c75843aa459f7a77f69043410fd36bf10449
jbrandt <jbrandt@nd.edu>
2019-11-21 14:45:27 -0500

    modified:   Exercise08.R

--- Exercise08.R
+++ Exercise08.R
@@ -1,2 +1,11 @@
 # to make UWvMSU_1-22-13.txt into a table/variable
 UWvMSU <- read.table("UWvMSU_1-22-13.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+# to make two variables for each team's data
+UW=UWvMSU[UWvMSU[,2]=="UW",]
+MSU=UWvMSU[UWvMSU[,2]=="MSU",]
+# to make both team variables into data frames
+as.data.frame(UW, row.names = NULL, optional = FALSE)
+as.data.frame(MSU, row.names = NULL, optional = FALSE)
+# to make a data frame for both teams, cumulative scores
+UWcum <- as.data.frame(cumsum(UW[,3]))
+MSUcum <- as.data.fram(cumsum(MSU[,3]))
