855bc288c2a2a92fb464abe1a713a29a0f2c83d6
jbrandt <jbrandt@nd.edu>
2019-11-21 17:27:46 -0500

    modified:   Exercise08.R

--- Exercise08.R
+++ Exercise08.R
@@ -6,6 +6,13 @@ MSU=UWvMSU[UWvMSU[,2]=="MSU",]
 # to make both team variables into data frames
 as.data.frame(UW, row.names = NULL, optional = FALSE)
 as.data.frame(MSU, row.names = NULL, optional = FALSE)
-# to make a data frame for both teams, cumulative scores
+# to make a data frame for both team's cumulative scores
 UWcum <- as.data.frame(cumsum(UW[,3]))
-MSUcum <- as.data.fram(cumsum(MSU[,3]))
+MSUcum <- as.data.frame(cumsum(MSU[,3]))
+# to make UWcum have the same number of rows as MSUcum
+UWcum[nrow(UWcum)+1,] <- 0
+UWcum[nrow(UWcum)+1,] <- 0
+UWcum[nrow(UWcum)+1,] <- 0
+UWcum[nrow(UWcum)+1,] <- 0
+# to combine UWcum and MSUcum into one data frame containing both cumulative scores
+UWMSUcum <- data.frame(UWcum, MSUcum)
